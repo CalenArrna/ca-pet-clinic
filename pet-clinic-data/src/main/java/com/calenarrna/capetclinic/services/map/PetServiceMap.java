e097c74af2294f8b5c29b6af972ec75cea67db1d
calenarrna <calenarrna@gmail.com>
2021-05-10 17:22:57 +0200

    Implemented Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/com/calenarrna/capetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.calenarrna.capetclinic.services.map;
+
+import com.calenarrna.capetclinic.model.Pet;
+import com.calenarrna.capetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.getById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(),pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
