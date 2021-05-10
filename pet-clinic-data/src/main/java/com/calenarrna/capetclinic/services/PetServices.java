88aa60dc3bf7c6d209a42ae57711e0c4649ff375
calenarrna <calenarrna@gmail.com>
2021-05-10 16:39:00 +0200

    Refactored service interfaces with a base interface. closes #11

--- pet-clinic-data/src/main/java/com/calenarrna/capetclinic/services/PetServices.java
+++ pet-clinic-data/src/main/java/com/calenarrna/capetclinic/services/PetServices.java
@@ -2,13 +2,7 @@ package com.calenarrna.capetclinic.services;
 
 import com.calenarrna.capetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetServices {
-    Pet findById (Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetServices extends CrudService<Pet, Long> {
 
 }
--- pet-clinic-data/src/main/java/com/calenarrna/capetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/calenarrna/capetclinic/services/VetService.java
@@ -2,13 +2,8 @@ package com.calenarrna.capetclinic.services;
 
 import com.calenarrna.capetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
 
-    Vet findById (Long id);
+public interface VetService extends CrudService<Vet, Long>{
 
-    Vet save(Vet vet);
-
-    Set<Vet> findAll ();
 }
