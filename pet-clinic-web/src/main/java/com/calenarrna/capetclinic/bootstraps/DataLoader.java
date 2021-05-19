4b312fd14da16bc10e65d51d23cccc07eb95fc5c
calenarrna <calenarrna@gmail.com>
2021-05-19 17:42:06 +0200

    Create Visit Spring Data JPA Service. Closes #48

--- pet-clinic-web/src/main/java/com/calenarrna/capetclinic/bootstraps/DataLoader.java
+++ pet-clinic-web/src/main/java/com/calenarrna/capetclinic/bootstraps/DataLoader.java
@@ -1,10 +1,7 @@
 package com.calenarrna.capetclinic.bootstraps;
 
 import com.calenarrna.capetclinic.model.*;
-import com.calenarrna.capetclinic.services.OwnerService;
-import com.calenarrna.capetclinic.services.PetTypeService;
-import com.calenarrna.capetclinic.services.SpecialtyService;
-import com.calenarrna.capetclinic.services.VetService;
+import com.calenarrna.capetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -70,6 +69,12 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
         ownerService.save(owner2);
 
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
         System.out.println("Loaded Owners!");
 
         Specialty radiology = new Specialty();
